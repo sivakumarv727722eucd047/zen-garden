aa5445101799fe7ca376bd70216b50b9fd853101
sivakumarv727722eucd047 <151495841+sivakumarv727722eucd047@users.noreply.github.com>
2023-11-30 19:20:30 +0530

    Add files via upload

---
+++ src/Welcome.jsx
@@ -0,0 +1,28 @@
+// Welcome.jsx
+
+import React from 'react';
+import './Welcome.css';
+import zenlogo from './zenlogo.jpg';
+
+export default function Welcome() {
+  return (
+    <div className="half-and-half-container">
+      <div className="left-half">
+        <img className="wellogo" src={zenlogo} alt="ZenGarden" />
+      </div>
+
+      <div className="right-half">
+        <p className="welcomepageMessage">
+          Welcome to ZenGarden,<br />Your oasis of calm in the digital world.<br />
+        </p>
+        
+        {/* Aesthetic Button */}
+        <div className="button-container">
+          <button className="go-to-home-button">Go to Home</button>
+          <button className="go-to-home-button">Login</button>
+          <button className="go-to-home-button">Sign Up</button>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+//import Articlepage from './articlepage';
+ //import Welcome from './Welcome';
+import Basictemplate from './basictemplate';
+import Test from './test';
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+   <Basictemplate/>
+  </React.StrictMode>
+);
+
+
